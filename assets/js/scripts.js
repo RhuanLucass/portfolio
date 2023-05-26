c157d0b33c5e72707caa27b2888c97a004888593
rhuanlucass <rhuanfernandes.rf@gmail.com>
2023-05-26 12:43:31 -0300

    Adicionada função para scroll suave da página ao clicar nos links do menu

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,6 +4,7 @@ const barTop = document.querySelector('.bar-top');
 const barBottom = document.querySelector('.bar-bottom');
 const mobile = document.querySelector('header nav');
 const links = document.querySelectorAll('header nav ul li a');
+const overlay = document.querySelector('.overlay');
 var heightMenu = 0;
 
 icon.addEventListener('click', transformIcon);
@@ -17,22 +18,22 @@ links.forEach((link) => {
   link.addEventListener("click", hideMenu);
 });
 
-// overlay.addEventListener("click", hideMenu);
+overlay.addEventListener("click", hideMenu);
 
 function hideMenu() {
   bar.classList.remove("rotate");
   barTop.classList.remove("rotate-top");
   barBottom.classList.remove("rotate-bottom");
-  mobile.classList.toggle("desktop-toggle");
-  // overlay.style.display = "none";
+  mobile.classList.remove("desktop-toggle");
+  overlay.style.visibility = "hidden";
 }
 
 function showMenu() {
   bar.classList.add("rotate");
   barTop.classList.add("rotate-top");
   barBottom.classList.add("rotate-bottom");
-  mobile.classList.toggle("desktop-toggle");
-  // overlay.style.display = "block";
+  mobile.classList.add("desktop-toggle");
+  overlay.style.visibility = "visible";
 }
 
 const html = document.querySelector('html');
@@ -47,17 +48,54 @@ function switchMode(){
   html.removeAttribute('data-theme');
 }
 
+const linksMenu = document.querySelectorAll('header nav ul li a[href^="#"]');
+let scrolled = false;
 
-// const container = document.querySelector('#home .image-main .image');
-// const imagem = document.querySelector('#home .image-main img');
+linksMenu.forEach((value) => value.addEventListener("click", scrollId));
 
-// container.addEventListener('mousemove', (e) => {
-//   const posX = e.clientX / container.offsetWidth;
-//   const posY = e.clientY / container.offsetHeight;
-  
-//   imagem.style.transform = `rotateY(${posX * 30 - 15}deg) rotateX(${-posY * 30 + 15}deg)`;
-// });
+function scrollId(e){
+  e.preventDefault();
 
-// container.addEventListener('mouseleave', () => {
-//   imagem.style.transform = 'rotate(0deg)';
-// });
+  if(scrolled === false){
+    const element = e.target;
+    const to = getScrollTop(element) - 50;
+
+    smoothScrollTo(to, 2000);
+    scrolled = true;
+    setTimeout(() => {
+      scrolled = false;
+      
+      document.querySelector('.current').classList.remove('current');
+      element.classList.add('current');
+    }, 1500);
+  }
+
+}
+
+function getScrollTop(element){
+  const id = element.getAttribute("href");
+  return document.querySelector(id).offsetTop;
+}
+
+function smoothScrollTo(endY, duration) {
+  const startY = window.scrollY || window.pageYOffset;
+  const distanceY = endY - startY;
+  const startTime = new Date().getTime();
+
+  duration = typeof duration !== "undefined" ? duration : 400;
+
+  const easeInOutQuart = (time, from, distance, duration) => {
+    if ((time /= duration / 2) < 1)
+      return (distance / 2) * time * time * time * time + from;
+    return (-distance / 2) * ((time -= 2) * time * time * time - 2) + from;
+  };
+
+  const timer = setInterval(() => {
+    const time = new Date().getTime() - startTime;
+    const newY = easeInOutQuart(time, startY, distanceY, duration);
+    if (time >= duration) {
+      clearInterval(timer);
+    }
+    window.scroll(0, newY);
+  }, 1000 / 60); // 60 fps
+}
